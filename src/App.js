68abceed429ea94e0ce4b3783ca6f8da545de502
Joshua Needham <joshuabneedham@gmail.com>
2021-10-07 20:56:09 -0500

    Tried to figure out the notifications some more. Getting CORS response on webhooks fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import { App as SendbirdApp } from 'sendbird-uikit'
+import { useState, useEffect } from 'react'
 import 'sendbird-uikit/dist/index.css'
 
 import './App.css';
-
 function App() {
+  const [webHooks] = useState(null)
   const APP_ID = '19C7D50E-9C23-4401-803B-6A916CA0C317'
   const USER_ID = 'Joshua Needham'
 
-  if (Notification.permission === 'granted') {
-    console.log(Notification.permission)
-  } else if (Notification.permission !== 'denied') {
-    Notification.requestPermission().then(permission => {
-      console.log(permission)
-    })
-  }
+  // useEffect(() => {
+  //   const webHooks = {
+  //     method: 'PUT',
+  //     headers: {
+  //       'user-agent': 'SendBird',
+  //       'Content-Type': 'application/json',
+  //       'x-signature': '{ x-signature }'
+  //     }
+  //   }
+
+  //   fetch('https://api-19C7D50E-9C23-4401-803B-6A916CA0C317.sendbird.com/v3/applications/settings/webhook', webHooks)
+  //     .then(response => response.json())
+  //     .then(data => console.log('Webhooks fetch response', data))
+  // }, [])
+
+  // if (Notification.permission === 'granted') {
+  //   console.log(Notification.permission)
+  // } else if (Notification.permission !== 'denied') {
+  //   Notification.requestPermission().then(permission => {
+  //     console.log(permission)
+  //   })
+  // }
 
 
   return (
     <div className="App">
+      {webHooks}
       <SendbirdApp appId={APP_ID} userId={USER_ID} />
     </div>
   );
