e95651b302710724c1356c08f862da0a0c242c97
Joshua Needham <joshuabneedham@gmail.com>
2021-10-11 22:47:57 -0500

    Pushing up some additional work.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,32 @@
 import { App as SendbirdApp } from 'sendbird-uikit'
-import { useState, useEffect } from 'react'
 import 'sendbird-uikit/dist/index.css'
+import WelcomeMessage from './WelcomeMessage.jsx'
+
+import {
+  APP_ID,
+  USER_ID,
+  NICKNAME
+} from './const.js'
 
 import './App.css';
 function App() {
-  const [webHooks] = useState(null)
-  const APP_ID = '19C7D50E-9C23-4401-803B-6A916CA0C317'
-  const USER_ID = 'Joshua Needham'
 
-  // useEffect(() => {
-  //   const webHooks = {
-  //     method: 'PUT',
-  //     headers: {
-  //       'user-agent': 'SendBird',
-  //       'Content-Type': 'application/json',
-  //       'x-signature': '{ x-signature }'
-  //     }
-  //   }
-
-  //   fetch('https://api-19C7D50E-9C23-4401-803B-6A916CA0C317.sendbird.com/v3/applications/settings/webhook', webHooks)
-  //     .then(response => response.json())
-  //     .then(data => console.log('Webhooks fetch response', data))
-  // }, [])
-
-  // if (Notification.permission === 'granted') {
-  //   console.log(Notification.permission)
-  // } else if (Notification.permission !== 'denied') {
-  //   Notification.requestPermission().then(permission => {
-  //     console.log(permission)
-  //   })
-  // }
+  if (Notification.permission === 'granted') {
+    console.log(Notification.permission)
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission().then(permission => {
+      console.log(permission)
+    })
+  }
 
 
   return (
     <div className="App">
-      {webHooks}
-      <SendbirdApp appId={APP_ID} userId={USER_ID} />
+      <WelcomeMessage />
+      <SendbirdApp
+        appId={APP_ID}
+        userId={USER_ID}
+        nickname={NICKNAME} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { APP_ID, USER_ID, NICKNAME } from './const';
+import { SendBirdProvider } from 'sendbird-uikit';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <SendBirdProvider appId={APP_ID} userId={USER_ID} nickname={NICKNAME}>
+    <App />
+  </SendBirdProvider>,
   document.getElementById('root')
 );
